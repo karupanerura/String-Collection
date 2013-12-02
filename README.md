0145470893c858d8a8baf4b61f432d0a94e025a9
karupanerura <karupa@cpan.org>
2013-12-02 16:00:34 +0900

    initialize commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+String::Collection - It's new $module
+
+# SYNOPSIS
+
+    use String::Collection;
+
+# DESCRIPTION
+
+String::Collection is ...
+
+# LICENSE
+
+Copyright (C) karupanerura.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+karupanerura <karupa@cpan.org>
---
+++ lib/String/Collection.pm
@@ -0,0 +1,39 @@
+package String::Collection;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+String::Collection - It's new $module
+
+=head1 SYNOPSIS
+
+    use String::Collection;
+
+=head1 DESCRIPTION
+
+String::Collection is ...
+
+=head1 LICENSE
+
+Copyright (C) karupanerura.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+karupanerura E<lt>karupa@cpan.orgE<gt>
+
+=cut
+
